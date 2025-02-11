f1f131fd828e2abd89f03e3ee95dbff7aa616606
Jagadees27 <jagadeeswaran2705@gmail.com>
2025-02-11 10:41:43 +0530

    initial home page

---
+++ src/utils/ServicesData.ts
@@ -0,0 +1,23 @@
+export const services = [
+  {
+    id: 1,
+    name: "Electronics",
+    description: "Get expert advice on electronics",
+    image:
+      "https://plus.unsplash.com/premium_photo-1679079456083-9f288e224e96?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Hotels",
+    description: "Get expert advice on hotels",
+    image:
+      "https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    name: "Automobiles",
+    description: "Get expert advice on automobiles",
+    image:
+      "https://plus.unsplash.com/premium_photo-1682147848371-c27b0726a84f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+      },
+      colors: {
+        primaryRed: "#ed2a4f",
+        bannerDark: "#aea7bf",
+        bannerLight: "#ccc7d6",
+      },
+    },
   },
   plugins: [],
 };
